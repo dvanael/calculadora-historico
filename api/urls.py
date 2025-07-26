5200278536638cd65f97c6e2fdd2870f5ee1447d
Ana Barbosa <anael.noki@gmail.com>
2025-07-26 17:24:35 -0300

    feat: adiciona viewsets e nested_urls

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path, include
 from rest_framework_nested import routers
-from . import views
+from . import viewsets
 
 router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet, basename="users")
-router.register(r"operations", views.OperationViewSet, basename="operations")
+router.register(r"users", viewsets.UserViewSet, basename="users")
+router.register(r"operations", viewsets.OperationViewSet, basename="operations")
 
 user_router = routers.NestedDefaultRouter(router, r"users", lookup="user")
-user_router.register(r"operations", views.OperationViewSet, basename="user-operations")
+user_router.register(
+    r"operations", viewsets.OperationViewSet, basename="user-operations"
+)
 
 urlpatterns = [
     path(r"", include(router.urls)),
     path(r"", include(user_router.urls)),
+    path(r"me/", viewsets.UserMeViewSet.as_view(), name="user-me"),
 ]
--- api/viewsets.py
+++ api/viewsets.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, views
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import UserSerializer, OperationSerializer
 from data.models import User, Operation
 
@@ -17,7 +19,21 @@ class OperationViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "post", "delete", "put"]
 
     def get_queryset(self):
-        id_user = self.kwargs.get("id_user")
+        id_user = self.kwargs.get("user_pk")
         if id_user:
             return super().get_queryset().filter(id_user=id_user)
         return super().get_queryset()
+
+
+class UserMeViewSet(views.APIView):
+    permission_classes = [IsAuthenticated]
+    http_method_names = ["get"]
+
+    def get(self, request, format=None):
+        data = {
+            "id": request.user.id,
+            "name": request.user.name,
+            "email": request.user.email,
+            "auth": request.user.is_authenticated,
+        }
+        return Response(data)
