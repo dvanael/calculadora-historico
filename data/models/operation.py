07a03680fc5bd8b95a5637ae090d39673336bbb8
Ana Barbosa <anael.noki@gmail.com>
2025-07-27 00:15:15 -0300

    feat: ordena operações por ultimo postado

--- data/models/operation.py
+++ data/models/operation.py
@@ -10,3 +10,6 @@ class Operation(models.Model):
     parameters = models.CharField(max_length=255)
     result = models.CharField(max_length=255)
     inclusion_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-inclusion_date"]
