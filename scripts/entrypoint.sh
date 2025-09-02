9225136ab661306ad947baa504520e4a97e2624f
Ana Barbosa <anael.noki@gmail.com>
2025-09-02 11:56:55 -0300

    feat: adiciona scripts para o docker

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+echo "Waiting for postgres..."
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+exec python manage.py runserver 0.0.0.0:8000
